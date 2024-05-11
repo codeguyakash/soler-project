b37b81a45f3743c6e5bb6e850e64436781f03a0a
Akash <codeguyakash@gmail.com>
2024-05-11 22:26:29 +0530

    create contact us page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -3,6 +3,8 @@ import Banner from "./../components/common/Banner";
 import SideNav from "../components/SideNav";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
+import GetInTouch from "./../components/GetInTouch";
+import contactusImage from "../assets/images/contactus.jpg";
 
 const ContactUs = () => {
   const [showSideNav, setShowSideNav] = useState(false);
@@ -12,21 +14,26 @@ const ContactUs = () => {
   return (
     <>
       <Nav showSideNavHandler={showSideNavHandler} />
-      <Banner title="From Service Page" />
+      <Banner title="Contact Us" />
       <SideNav
         showSideNavHandler={showSideNavHandler}
         showSideNav={showSideNav ? "block" : "none"}
       />
-      <section className="bg-gray-100 py-16 lg:py-24">
+      <section
+        className="bg-gray-100 py-20 px-10 relative bg-cover bg-no-repeat"
+        style={{ backgroundImage: `url(${contactusImage})` }}
+      >
         <div className="container mx-auto px-4">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="text-center">
-              <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-                under maintenance...
+              <h2 className="text-5xl py-5 text-center text-white font-bold">
+                Say Hey 👋
               </h2>
             </div>
           </div>
+          {/* <div className="absolute bg-black w-full h-full opacity-45"></div> */}
         </div>
+        <GetInTouch />
       </section>
       <Footer />
     </>
