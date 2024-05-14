2b29cd0f24a014d292333e0dca270b7843697241
Akash <codeguyakash@gmail.com>
2024-05-14 21:05:09 +0530

    why-solar-go

--- src/pages/Faqs.jsx
+++ src/pages/Faqs.jsx
@@ -13,89 +13,105 @@ const Faqs = () => {
   };
   const faqs = [
     {
+      id: 1,
       question: "What is a Grid Connected Rooftop Solar PV System?",
       answer:
         "In grid connected rooftop small Solar Photo Voltaic (SPV) system, the DC (Direct Current) power generated from solar panel is converted to AC (Alternating Current) power using power conditioning unit/Inverter and is fed to the grid. Conversion to AC is desirable as AC is the most commonly used and most preferred electric power for household equipment, office, and buildings, etc. The major use of DC is to supply power to electrical devices and also to charge batteries.",
     },
     {
+      id: 2,
       question:
         "What are the main components of a Grid Connected Rooftop SPV system?",
       answer:
         "Solar PV Modules/Solar Panels, Inverter, Module mounting structure, Bi-direction Meters, Balance of System.",
     },
     {
+      id: 3,
       question:
         "How much area is required for a One kWp (kilowatt peak) rooftop Solar PV system?",
       answer:
         "About 10-12 Sq.mt of shadow-free area is required to set up One kWp Grid Connected Rooftop Solar PV System.",
     },
     {
+      id: 4,
       question:
         "What types of roofs are suitable for Rooftop Solar (RTS) system?",
       answer:
         "Rooftop SPV systems can be installed on any type of roof having sufficient load bearing capacity.",
     },
     {
+      id: 5,
       question:
         "What is the daily energy generated from a One kWp Solar power plant?",
       answer:
         "On a clear sunny day, One kWp solar power plant can generate 4 to 5.5 units in a day.",
     },
     {
+      id: 6,
       question: "What are the advantages of Grid Connected Rooftop SPV system?",
       answer:
         "Saving on electricity bill by the consumer, Utilization of available vacant roof space; no additional land required, Low gestation period, No additional requirement of transmission and distribution (T&D) lines, Long-term energy and ecological security by reduction in carbon emission, Better management of daytime peak loads by DISCOM/ utility.",
     },
     {
+      id: 7,
       question:
         "What is the average cost of a Grid Connected Rooftop SPV system?",
       answer:
         "The current benchmark cost of grid connected rooftop solar systems can be seen in notification section at https://solarrooftop.gov.in/notifications/view.",
     },
     {
+      id: 8,
       question:
         "What are the subsidies/ capital support available from the Government?",
       answer:
         "Central Financial Assistance (CFA) is available only for residential sector grid connected solar rooftop projects. For other sectors e.g. government, institutional, social, commercial, industrial etc., CFA is not available.",
     },
     {
+      id: 9,
       question: "Is there any Subsidy/Support from State Government?",
       answer:
         "Information on state subsidies can be seen on the website of the concerned electricity distribution company. The rooftop solar portals of electricity distribution companies can be assessed at: https://solarrooftop.gov.in/grid_others/discomPortalLink.",
     },
     {
+      id: 10,
       question: "What is the gross potential of solar power in the country?",
       answer:
         "India is endowed with vast solar energy potential. About 5,000 trillion kWh per year energy is incident over India’s land area with most parts receiving 3-5 kWh per sq. m per day. Based upon the availability of land and solar radiation, the potential of solar power in the country has been assessed to be around 750 GW.",
     },
     {
+      id: 11,
       question: "What is the potential for rooftop solar power in the country?",
       answer:
         "National Institute of Solar Energy (NISE), an autonomous institute under the Ministry of New and Renewable Energy, has estimated a potential of 43 GW for Grid Connected Rooftop Solar in the country.",
     },
     {
+      id: 12,
       question:
         "What targets has the Government kept for installation of Rooftop Solar Plants in the country?",
       answer:
         "Government of India has set the target of installing 40,000 MW of Rooftop Solar Power by the year 2022. For State-wise tentative targets, click here.",
     },
     {
+      id: 13,
       question:
         "What is the State-wise breakup of sanctioned capacity under the Phase-II RTS scheme?",
       answer:
         "To know about the details on capacity sanctioned under Phase-II of RTS scheme, click here.",
     },
     {
+      id: 14,
       question: "What is net-metering?",
       answer:
         "All SPV systems generate power only during daytime when sun is available. In net-metered systems, the generated power is utilized for self-consumption, and excess power is exported to the grid as long as grid is available. In case, where solar power is not sufficient due to cloud cover etc., power is drawn from the grid to power the loads. A bi-directional or net meter records the energy flow in both the directions and at the end of billing period net energy used is calculated. The beneficiary has to pay for only the net energy used.",
     },
     {
+      id: 15,
       question: "What is net-billing?",
       answer:
         "Connections in net-billing RTS are similar to net-metering. However, at the end of the billing cycle (normally a month) any excess energy in the grid shall not be carried forward like in net metering, but shall be purchased by the DISCOM as per pre-decided tariff. Therefore, energy banking in the grid is only within a billing cycle.",
     },
     {
+      id: 16,
       question: "How can I apply for installation of solar rooftop system?",
       answer:
         "Residential consumers and Group Housing Society can apply for installation of solar rooftop system through the online portal of DISCOMs. The link for the online portal of DISCOMs is https://solarrooftop.gov.in/grid_others/discomPortalLink.",
@@ -107,66 +123,78 @@ const Faqs = () => {
   };
   return (
     <>
-      <Nav showSideNavHandler={showSideNavHandler} />
-      <Banner
-        title="Solar FAQ Hub"
-        tagline="Illuminate Your Knowledge: Answers to Your Solar Questions"
-      />
-      <SideNav
-        showSideNavHandler={showSideNavHandler}
-        showSideNav={showSideNav ? "block" : "none"}
-      />
-
-      <section className="bg-gray-100 pb-16 lg:pb-24 w-auto">
+      <div className="bg-gray-100">
+        <Nav showSideNavHandler={showSideNavHandler} />
+        <Banner
+          title="Solar FAQ Hub"
+          tagline="Illuminate Your Knowledge: Answers to Your Solar Questions"
+        />
+        <SideNav
+          showSideNavHandler={showSideNavHandler}
+          showSideNav={showSideNav ? "block" : "none"}
+        />
         <div
-          className="h-96 flex items-center justify-center flex-col relative bg-no-repeat bg-center"
+          className="h-96 bg-center bg-cover bg-no-repeat relative flex items-center justify-center px-4"
           style={{ backgroundImage: `url(${imagefour})` }}
         >
           <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
             Frequently Asked Questions
           </h2>
-          {/* <div className="absolute bg-black w-full h-full opacity-25"></div> */}
+          <div className="absolute bg-black w-full h-full opacity-25"></div>
         </div>
-        <div className="container mx-auto ">
-          <div className="max-w-7xl mx-auto ">
-            <div className="text-center">
-              <div className="mt-8">
-                {faqs.map((faq, index) => (
-                  <div key={index} className="mb-4">
-                    <button
-                      className="flex justify-between items-center w-full bg-white p-4 rounded-lg shadow-md focus:outline-none"
-                      onClick={() => toggleAccordion(index)}
+        <section className="bg-gray-100 pb-16 lg:pb-24 w-auto px-4">
+          {/* <div
+          className="h-96 flex items-center justify-center flex-col relative bg-no-repeat bg-center"
+          style={{ backgroundImage: `url(${imagefour})` }}
+        >
+
+        </div> */}
+          <div className="container mx-auto ">
+            <div className="max-w-7xl mx-auto ">
+              <div className="text-center">
+                <div className="mt-8">
+                  {faqs.map((faq, index) => (
+                    <div
+                      key={index}
+                      className="mb-4 rounded-lg shadow-md bg-white p-4"
                     >
-                      <strong>{faq.question}</strong>
-                      <svg
-                        className={`h-6 w-6 transition-transform ${
-                          openIndex === index ? "rotate-180" : ""
-                        }`}
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
+                      <button
+                        className="flex justify-between items-center w-full bg-white rounded-lg focus:outline-none"
+                        onClick={() => toggleAccordion(index)}
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M19 9l-7 7-7-7"
-                        />
-                      </svg>
-                    </button>
-                    {openIndex === index && (
-                      <p className="mt-2 text-lg text-gray-700 text-start">
-                        {faq.answer}
-                      </p>
-                    )}
-                  </div>
-                ))}
+                        <strong className="text-[18px]">
+                          Q {faq.id}. {faq.question}
+                        </strong>
+                        <svg
+                          className={`h-6 w-6 transition-transform ${
+                            openIndex === index ? "rotate-180" : ""
+                          }`}
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M19 9l-7 7-7-7"
+                          />
+                        </svg>
+                      </button>
+                      {openIndex === index && (
+                        <p className="mt-2 text-[16px] text-gray-700 text-start">
+                          <span className="font-bold">Ans.</span> {faq.answer}
+                        </p>
+                      )}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <Footer />
+        </section>
+        <Footer />
+      </div>
     </>
     // <>
     //   <Nav showSideNavHandler={showSideNavHandler} />
--- src/pages/WhyGoSolar.jsx
+++ src/pages/WhyGoSolar.jsx
@@ -3,31 +3,104 @@ import Banner from "./../components/common/Banner";
 import SideNav from "../components/SideNav";
 import Nav from "../components/Nav";
 import Footer from "./../components/Footer";
+import imagefour from "../assets/images/faq-banner.jpg";
+import SolarAdvantage from "./../components/SolarAdvantage";
 
 const WhyGoSolar = () => {
   const [showSideNav, setShowSideNav] = useState(false);
   const showSideNavHandler = () => {
     setShowSideNav(!showSideNav);
   };
+  const contentArray = [
+    {
+      id: 1,
+      title: "Save Money",
+      description:
+        "Save up to 50% on electricity bills with Tata Power solar panels. Cost savings are substantial, with residential customers saving over Rs 10 lakh+ and MSMEs saving over Rs 2 Cr+.",
+    },
+    {
+      id: 2,
+      title: "Long Lifetime",
+      description:
+        "Solar power systems have a long lifetime, lasting over 25 years.",
+    },
+    {
+      id: 3,
+      title: "Quick Payback",
+      description:
+        "Experience quick payback on your investment, with a typical payback period of 4-5 years.",
+    },
+    {
+      id: 4,
+      title: "Grid Independence",
+      description:
+        "Beat the ever-increasing electricity tariffs and achieve grid independence.",
+    },
+    {
+      id: 5,
+      title: "Minimal Maintenance",
+      description:
+        "Solar panels require minimal maintenance as they have no moving parts, resulting in low maintenance costs.",
+    },
+    {
+      id: 6,
+      title: "Scalable Design",
+      description:
+        "Enjoy easy upgrades for evolving energy needs with Tata Power's scalable solar panel design.",
+    },
+    {
+      id: 7,
+      title: "Tax Benefits",
+      description:
+        "Businesses can benefit from up to 40% accelerated tax depreciation in the first year of commissioning the solar plant.",
+    },
+    {
+      id: 8,
+      title: "Clean & Green Energy",
+      description:
+        "Solar plants provide clean and green energy for MSMEs, homes, and large enterprises. They can potentially abate thousands of tons of CO2 emissions annually, equivalent to planting thousands of trees.",
+    },
+    {
+      id: 9,
+      title: "Green Building Certification",
+      description:
+        "Earn crucial points for sustainable building design certifications like IGBC and LEED by incorporating solar power.",
+    },
+  ];
   return (
     <>
       <Nav showSideNavHandler={showSideNavHandler} />
-      <Banner title="From Why Go Solar Page" />
+      <Banner
+        title="Empowering Tomorrow: Discover the Solar Advantage"
+        tagline="Unleash Savings, Embrace Sustainability, Go Solar Today!"
+      />
       <SideNav
         showSideNavHandler={showSideNavHandler}
         showSideNav={showSideNav ? "block" : "none"}
       />
-       <section className="bg-gray-100 py-16 lg:py-24">
-        <div className="container mx-auto px-4">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="text-center">
-              <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-                under maintenance...
-              </h2>
+      <div
+        className="h-96 bg-center bg-cover bg-no-repeat relative flex items-center justify-center px-4"
+        style={{ backgroundImage: `url(${imagefour})` }}
+      >
+        <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
+          Benefits Of Going Solar
+        </h2>
+        <div className="absolute bg-black w-full h-full opacity-25"></div>
+      </div>
+      <section className="bg-gray-100 py-16 lg:py-24">
+        <div className="container mx-auto px-4 grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3  items-center justify-center gap-5">
+          {contentArray.map((content) => (
+            <div
+              key={content.id}
+              className="shadow-lg p-10 min-h-64 border rounded-lg"
+            >
+              <h2 className="text-2xl font-semibold">{content.title}</h2>
+              <p className="text-lg my-2">{content.description}</p>
             </div>
-          </div>
+          ))}
         </div>
       </section>
+      <SolarAdvantage />
       <Footer />
     </>
   );
