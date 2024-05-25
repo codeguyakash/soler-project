ec8bf9ab10d924c221237ccf847af7f49cc4a737
Akash <codeguyakash@gmail.com>
2024-05-25 13:47:03 +0530

    lazy load components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Products from "./pages/Products";
-import Blogs from "./pages/muftBijliYojana";
-import WhyGoSolar from "./pages/WhyGoSolar";
-import SolarSurvey from "./pages/SolarSurvey";
-import SolarCalculator from "./pages/SolarCalculator";
-import ContactUs from "./pages/ContactUs";
-import Faqs from "./pages/Faqs";
-import Services from "./pages/Services";
+import { lazy, Suspense } from "react";
+import Loader from "./pages/Loader";
+
+// Lazy loading components
+const Home = lazy(() => import("./pages/Home"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Products = lazy(() => import("./pages/Products"));
+const Blogs = lazy(() => import("./pages/muftBijliYojana"));
+const WhyGoSolar = lazy(() => import("./pages/WhyGoSolar"));
+const SolarSurvey = lazy(() => import("./pages/SolarSurvey"));
+const SolarCalculator = lazy(() => import("./pages/SolarCalculator"));
+const ContactUs = lazy(() => import("./pages/ContactUs"));
+const Faqs = lazy(() => import("./pages/Faqs"));
+const Services = lazy(() => import("./pages/Services"));
+
 function App() {
   return (
-    <>
-      <Router>
+    <Router>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/solar-survay" element={<SolarSurvey />} />
@@ -28,8 +33,8 @@ function App() {
           <Route path="/muft-bijli-yojana" element={<Blogs />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </Router>
-    </>
+      </Suspense>
+    </Router>
   );
 }
 
