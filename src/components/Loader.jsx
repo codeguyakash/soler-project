b662299299275ba4c114a80c198565a9961decfa
Akash <codeguyakash@gmail.com>
2024-07-29 23:10:01 +0530

    fix ui

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,7 @@
 const Loader = () => {
+  const bg = "bg-black bg-opacity-50";
   return (
-    <div className="flex items-center justify-center h-full w-full absolute top-0 left-0 bg-black bg-opacity-50 z-10">
+    <div className="flex items-center justify-center h-full w-full absolute top-0 left-0  z-10">
       <div className="border-[2px] border-t-[#1e40af] animate-spin animate-duration-200 border-[#10b981] w-12 h-12 rounded-full"></div>
     </div>
   );
